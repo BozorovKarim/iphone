938e87c7bfe486e11eafbf5bd3dc531687e7bfd0
COPP <COPP@rce.rce>
2023-06-03 09:21:38 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.burger-btn').addEventListener('click', () =>{
+    document.querySelector('.header__burger-block').classList.toggle('open');
+    document.querySelector('.burger-btn').classList.toggle('open');
+})
